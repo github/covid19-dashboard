3d843c79a139a07f5943dd6a5d42c5906dd0be80
Hamel Husain <hamel.husain@gmail.com>
2020-03-23 12:36:36 -0700

    update logic

--- _action_files/run_notebooks.sh
+++ _action_files/run_notebooks.sh
@@ -19,8 +19,8 @@ done
 # Emit Errors If Exists So Downstream Task Can Open An Issue
 if [ -z "$ERRORS" ]
 then
+    echo "::set-output name=error_bool::false"
+else
     echo "::set-output name=error_bool::true"
     echo "::set-output name=error_str::${ERRORS}"
-else
-    echo "::set-output name=error_bool::false"
 fi
