ec1d3d8e279f0ab305fc325d06318bf06417dd38
Hamel Husain <hamel.husain@gmail.com>
2020-04-03 00:06:51 -0700

    update script

--- _action_files/run_notebooks.sh
+++ _action_files/run_notebooks.sh
@@ -7,7 +7,7 @@ ERRORS=""
 
 for file in *.ipynb
 do
-    if [[ "${file}" == "2020-03-16-covid19_growth_bayes.ipynb" ]]; then
+    if [ "${file}" = "2020-03-16-covid19_growth_bayes.ipynb" ]; then
         echo "Skipping ${file}"
     elif papermill --kernel python3 "${file}" "${file}"; then
         echo "Sucessfully refreshed ${file}\n\n\n\n"
